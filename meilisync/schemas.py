b1a5ca0e2ce40d0fd30ec5e888ccd98ac6cc0d07
long2ice <long2ice@gmail.com>
2023-11-30 14:43:37 +0800

    feat: support date

--- meilisync/schemas.py
+++ meilisync/schemas.py
@@ -20,7 +20,8 @@ class Event(ProgressEvent):
         for k, v in self.data.items():
             if isinstance(v, datetime.datetime):
                 v = int(v.timestamp())
-
+            elif isinstance(v, datetime.date):
+                v = str(v)
             if fields_mapping is not None and k in fields_mapping:
                 real_k = fields_mapping[k] or k
                 data[real_k] = v
